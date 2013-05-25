eb08bb2854e6c92ffda6acb3ae2b45b98b7b256d
jannewaren <janne.waren@iki.fi>
2013-05-25 23:21:52 +0300

    Create bootstrap-wysihtml5.fi-FI.js
    Add Finnish language translations

---
+++ src/locales/bootstrap-wysihtml5.fi-FI.js
@@ -0,0 +1,51 @@
+/**
+ * Finnish translation for bootstrap-wysihtml5
+ */
+(function($){
+    $.fn.wysihtml5.locale["fi-FI"] = {
+        font_styles: {
+            normal: "Normaali teksti",
+            h1: "Otsikko 1",
+            h2: "Otsikko 2",
+            h3: "Otsikko 3",
+            h4: "Otsikko 4",
+            h5: "Otsikko 5",
+            h6: "Otsikko 6"
+        },
+        emphasis: {
+            bold: "Lihavoitu",
+            italic: "Kursivoitu",
+            underline: "Alleviivattu"
+        },
+        lists: {
+            unordered: "Järjestämätön lista",
+            ordered: "Järjestetty lista",
+            outdent: "Suurenna sisennystä",
+            indent: "Pienennä sisennystä"
+        },
+        link: {
+            insert: "Lisää linkki",
+            cancel: "Peruuta"
+        },
+        image: {
+            insert: "Lisää kuva",
+            cancel: "Peruuta"
+        },
+        html: {
+            edit: "Muokkaa HTML"
+        },
+        colours: {
+            black: "Musta",
+            silver: "Hopea",
+            gray: "Harmaa",
+            maroon: "Kastanjanruskea",
+            red: "Punainen",
+            purple: "Lila",
+            green: "Vihreä",
+            olive: "Oliivinvihreä",
+            navy: "Tummansininen",
+            blue: "Sininen",
+            orange: "Oranssi"
+        }
+    };
+}(jQuery));
